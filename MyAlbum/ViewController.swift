72cc8f43ad9cb5735c2e551bce00e47a16c2f04c
sssssungs <32057999+sssssungs@users.noreply.github.com>
2020-03-11 10:27:25 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by Randy Won on 2020/03/11.
+//  Copyright © 2020 Randy Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
